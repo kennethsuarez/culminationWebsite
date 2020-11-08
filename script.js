a8987d18c3245920efd18c3156d78ac0a6987e32
kennethsuarez <klsuarez1@up.edu.ph>
2020-11-08 22:31:05 +0800

    fixed formatting of time and nav bar

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const zeroPad = (num, places) => String(num).padStart(places, '0')
 
 function update_time() {
+    
     // get new date and time
     var today = new Date();
     var hour = zeroPad(today.getHours(), 2);
     var minute = zeroPad(today.getMinutes(), 2);
     var second = zeroPad(today.getSeconds(), 2);
+    
     // update background color
     var new_bg_color = "#" + hour + minute + second;
     document.body.style.backgroundColor = new_bg_color;
+
     // update tag with class
     var display_time = zeroPad(hour, 2) + " " + minute  + " " + second
     document.getElementById("curr-time").innerHTML = "#" + display_time;
